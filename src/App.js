12d5274bfa03483318978de66af29832be8bb1c8
akhio <aidos.akzhol05@gmail.com>
2022-07-12 19:59:20 +0600

    graph-background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { GraphPaper } from './components';
 
 function App() {
   return (
     <div>
-      
+      <div className='game'> 
+        <div className='game__graphBackground'>
+          <GraphPaper />
+        </div>
+      </div>
+      <div className='control'>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as GraphPaper } from './graphPaper/index'
